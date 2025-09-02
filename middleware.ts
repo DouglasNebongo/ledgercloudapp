d4136ef948016ce26cb7620d6a2e268dbde11b7e
FermatD <mnebongo@gmail.com>
2025-09-02 12:10:24 -0400

    -

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
+export const runtime = 'nodejs';
 
 const requestCounts = new Map<string, { count: number, timestamp: number }>();
 
